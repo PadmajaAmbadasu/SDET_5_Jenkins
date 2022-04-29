ef7dc5900c195ee773f92697e8e1676f1e9551f5
PadmajaAmbadasu <padmajaambadasu@gmail.com>
2022-04-29 12:38:15 +0530

    commiting

---
+++ MavenSample_SDET5/src/test/java/MavenProfiling/ReadDatafromCmdTest.java
@@ -0,0 +1,22 @@
+package MavenProfiling;
+import org.testng.annotations.Test;
+
+public class ReadDatafromCmdTest {
+	
+	@Test(groups="smoke")
+	public void readData() {
+		
+		System.out.println("read Data from CommandLine");
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("username"));
+		System.out.println(System.getProperty("password"));
+	}
+
+@Test(groups="regression")
+public void testdata() {
+	
+	System.out.println("read data from cmd");
+}
+	
+	
+}
---
+++ MavenSample_SDET5/src/test/java/newClassTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class newClassTest {
+	
+	@Test
+	
+	public void newClass() {
+		System.out.println("hii Everyone");
+		
+	}
+
+}
